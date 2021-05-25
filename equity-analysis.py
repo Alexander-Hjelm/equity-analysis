e2695ddac571f82b01cfb469cd0f144e546faf7e
Alexander Hjelm <alexander.hjelm@solidify.dev>
2021-05-25 18:38:46 +0200

    Store retrieval dates, merge with existing data

--- equity-analysis.py
+++ equity-analysis.py
@@ -4,6 +4,7 @@ import yfinance as yf
 import plotly.graph_objects as go
 from datetime import datetime
 import pandas
+import json
 
 def plot_candlestick(data: pandas.DataFrame):
     fig = go.Figure(data=[go.Candlestick(
@@ -23,28 +24,50 @@ def plot_candlestick(data: pandas.DataFrame):
 def get_data_filename(stock):
     return "data/{}".format(stock)
 
-def download_stock_data(stocks, start_time_str):
+def download_stock_data(stocks):
+    # Get retrieval dates
+    with open('data/retr_dates.json', 'r') as f:
+        retr_data=f.read()
+    retr_dates = json.loads(retr_data)
+
     now = datetime.now()
     now_str = str(now.year) + '-' + str(now.month) + '-' + str(now.day)
 
     for st in stocks:
+        if st in retr_dates:
+            start_str = str(retr_dates[st])
+        else:
+            start_str = "2020-01-01"
+
         # Get the data for the stock Apple by specifying the stock ticker, start date, and end date
-        data = yf.download(st, start_time_str, now_str)
+        data = yf.download(st, start_str, now_str)
         
+        # Join with existing data
+        data2 = pandas.read_csv(get_data_filename(st))
+        data = data.append(data2)
+
+
+
+        # Save to file
         file_name = get_data_filename(st)
         data.to_csv(file_name)
         #print(data)
-        
-    # TODO: Store/Get the latest retrieval time in a separate json file
+    
+        retr_dates[st] = now_str
+    
+    # Store retrieval dates
+    with open("data/retr_dates.json", "w") as f:
+        f.write(json.dumps(retr_dates))
 
 
 
 with open('input_stocks') as f:
     input_stocks = [line.rstrip() for line in f]
 
-download_stock_data(input_stocks, '2020-01-01')
+download_stock_data(input_stocks)
 
 for st in input_stocks:
+    file_name = get_data_filename(st)
     data2 = pandas.read_csv(file_name)
 
     plot_candlestick(data2)
